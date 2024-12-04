3cc565e8b75f5741506acc3164cb46a2cccca58d
ckh <ckh@atguigu.com>
2024-12-04 15:08:19 +0800

    完成结算

---
+++ spzx-api/spzx-api-cart/src/main/java/com/spzx/cart/api/factory/RemoteCartFallbackFactory.java
@@ -0,0 +1,32 @@
+package com.spzx.cart.api.factory;
+
+import com.spzx.cart.api.domain.CartInfo;
+import com.spzx.common.core.domain.R;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+public class RemoteCartFallbackFactory implements FallbackFactory<RemoteCartService> {
+    private static final Logger log = LoggerFactory.getLogger(RemoteCartFallbackFactory.class);
+
+    @Override
+    public RemoteCartService create(Throwable throwable)
+    {
+
+        return new RemoteCartService()
+        {
+
+            @Override
+            public R<List<CartInfo>> getCartCheckedList(Long userId, String source) {
+                return R.fail("获取用户购物车选中数据失败:" + throwable.getMessage());
+            }
+
+        };
+    }
+
+}
---
+++ spzx-api/spzx-api-cart/src/main/java/com/spzx/cart/api/factory/RemoteCartService.java
@@ -0,0 +1,18 @@
+package com.spzx.cart.api.factory;
+
+import com.spzx.cart.api.domain.CartInfo;
+import com.spzx.cart.api.factory.RemoteCartFallbackFactory;
+import com.spzx.common.core.constant.SecurityConstants;
+import com.spzx.common.core.domain.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
+
+@FeignClient(contextId = "remoteCartService",value = "spzx-cart", fallbackFactory = RemoteCartFallbackFactory.class)
+public interface RemoteCartService {
+    @GetMapping("/getCartCheckedList/{userId}")
+    public R<List<CartInfo>> getCartCheckedList(@PathVariable("userId") Long userId, @RequestHeader(SecurityConstants.FROM_SOURCE)String source);
+}
